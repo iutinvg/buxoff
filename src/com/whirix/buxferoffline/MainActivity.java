d0f90efd85b69ebb98e0379cffe1638deace41d7
Vyacheslav Iutin <iutin@whirix.com>
2012-08-13 06:01:32 +0700

    enahncement: add account field

--- src/com/whirix/buxferoffline/MainActivity.java
+++ src/com/whirix/buxferoffline/MainActivity.java
@@ -21,10 +21,12 @@ public class MainActivity extends Activity {
 	private final static String SAVED_EMAIL = "com.whirix.buxoff.email";
 	private final static String SAVED_LAST_TAGS = "com.whirix.buxoff.tags";
 	private final static String SAVED_LAST_DESC = "com.whirix.buxoff.desc";
+	private final static String SAVED_LAST_ACCT = "com.whirix.buxoff.acct";
 
 	// to store the tags
-	private final static String PREF_TAGS = "com.whirix.buxoff.all_tags";
 	private final static String PREF_DESC = "com.whirix.buxoff.all_desc";
+	private final static String PREF_TAGS = "com.whirix.buxoff.all_tags";
+	private final static String PREF_ACCT = "com.whirix.buxoff.all_accounts";
 	private final static String PREF_RULES = "com.whirix.buxoff.rules";
 
 	@Override
@@ -33,8 +35,9 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 
 		initEmailHandlers();
-		initTagsHandlers();
 		initDescHandlers();
+		initTagsHandlers();
+		initAcctHandlers();
 		setupAdapters();
 	}
 
@@ -138,15 +141,46 @@ public class MainActivity extends Activity {
 		});
 	}
 
+	protected void initAcctHandlers() {
+		AutoCompleteTextView acct = (AutoCompleteTextView) findViewById(R.id.edit_acct);
+
+		// set saved value
+		final SharedPreferences sp = getSharedPreferences(PREF_FILE,
+				MODE_PRIVATE);
+		acct.setText(sp.getString(SAVED_LAST_ACCT, ""));
+
+		acct.addTextChangedListener(new TextWatcher() {
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+			}
+
+			@Override
+			public void afterTextChanged(Editable s) {
+				SharedPreferences.Editor edit = sp.edit();
+				edit.putString(SAVED_LAST_ACCT, s.toString());
+				edit.commit();
+			}
+		});
+	}
+
 	public void sendMessage(View view) {
 		EditText email = (EditText) findViewById(R.id.edit_email);
 		EditText amount = (EditText) findViewById(R.id.edit_amount);
 		AutoCompleteTextView tags = (AutoCompleteTextView) findViewById(R.id.edit_tags);
 		AutoCompleteTextView desc = (AutoCompleteTextView) findViewById(R.id.edit_desc);
+		AutoCompleteTextView acct = (AutoCompleteTextView) findViewById(R.id.edit_acct);
 
 		// save history before sending
-		tagsHistory(tags.getText().toString());
-		descHistory(desc.getText().toString());
+		autocompletes(desc.getText().toString(), PREF_DESC);
+		autocompletes(tags.getText().toString(), PREF_TAGS);
+		autocompletes(acct.getText().toString(), PREF_ACCT);
 		rule(desc.getText().toString(), tags.getText().toString());
 
 		// update autocomplete adapters with new tags/descriptions
@@ -159,7 +193,7 @@ public class MainActivity extends Activity {
 		emailIntent.putExtra(Intent.EXTRA_SUBJECT, "expense");
 		emailIntent.putExtra(Intent.EXTRA_TEXT, desc.getText().toString() + " "
 				+ amount.getText().toString() + " tags:"
-				+ tags.getText().toString() + " acct:cash");
+				+ tags.getText().toString() + " acct:" + acct.getText().toString());
 		emailIntent.setType("plain/text");
 		startActivity(Intent.createChooser(emailIntent,
 				getString(R.string.email_prompt)));
@@ -168,39 +202,27 @@ public class MainActivity extends Activity {
 	protected void setupAdapters() {
 		AutoCompleteTextView tags = (AutoCompleteTextView) findViewById(R.id.edit_tags);
 		AutoCompleteTextView desc = (AutoCompleteTextView) findViewById(R.id.edit_desc);
+		AutoCompleteTextView acct = (AutoCompleteTextView) findViewById(R.id.edit_acct);
 
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, descHistory(null));
+				android.R.layout.simple_list_item_1, autocompletes(null, PREF_DESC));
 		desc.setAdapter(adapter);
 
 		adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, tagsHistory(null));
+				android.R.layout.simple_list_item_1, autocompletes(null, PREF_TAGS));
 		tags.setAdapter(adapter);
+
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, autocompletes(null, PREF_ACCT));
+		acct.setAdapter(adapter);
 	}
 
-	protected String[] tagsHistory(String new_tag) {
-		final SharedPreferences sp = getSharedPreferences(PREF_TAGS,
-				MODE_PRIVATE);
+	protected String[] autocompletes(String new_item, String pref_id) {
+		final SharedPreferences sp = getSharedPreferences(pref_id, MODE_PRIVATE);
 
-		if (new_tag != null) {
+		if (new_item != null) {
 			SharedPreferences.Editor edit = sp.edit();
-			edit.putString(new_tag, "");
-			edit.commit();
-		}
-
-		Map<String, ?> all = sp.getAll();
-		Set<String> keys = all.keySet();
-
-		return keys.toArray(new String[keys.size()]);
-	}
-
-	protected String[] descHistory(String new_desc) {
-		final SharedPreferences sp = getSharedPreferences(PREF_DESC,
-				MODE_PRIVATE);
-
-		if (new_desc != null) {
-			SharedPreferences.Editor edit = sp.edit();
-			edit.putString(new_desc, "");
+			edit.putString(new_item, "");
 			edit.commit();
 		}
 
