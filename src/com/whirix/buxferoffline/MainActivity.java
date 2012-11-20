6c2736d4022206ed75aaf8fa6da94578dcb0e73f
iutinvg <iutin@whirix.com>
2012-11-20 09:08:04 +0700

    optimize layout; enter several transactions before sending them

--- src/com/whirix/buxferoffline/MainActivity.java
+++ src/com/whirix/buxferoffline/MainActivity.java
@@ -1,13 +1,17 @@
 package com.whirix.buxferoffline;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
+import java.util.Date;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -17,19 +21,21 @@ import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 
 public class MainActivity extends FragmentActivity {
+	private final static String TAG = "MainActivity";
 
 	private final static String PREF_FILE = "com.whirix.buxoff.pref";
 	private final static String SAVED_EMAIL = "com.whirix.buxoff.email";
 	private final static String SAVED_LAST_TAGS = "com.whirix.buxoff.tags";
 	private final static String SAVED_LAST_DESC = "com.whirix.buxoff.desc";
 	private final static String SAVED_LAST_ACCT = "com.whirix.buxoff.acct";
+	private final static String SAVED_TRANSACTIONS = "com.whirix.buxoff.transactions";
 
 	// to store the tags
 	private final static String PREF_DESC = "com.whirix.buxoff.all_desc";
 	private final static String PREF_TAGS = "com.whirix.buxoff.all_tags";
 	private final static String PREF_ACCT = "com.whirix.buxoff.all_accounts";
 	private final static String PREF_RULES = "com.whirix.buxoff.rules";
-	
+
 	// the error message to show in validation error dialog
 	private String validation_error;
 
@@ -53,6 +59,13 @@ public class MainActivity extends FragmentActivity {
 
 	protected void initEmailHandlers() {
 		EditText email = (EditText) findViewById(R.id.edit_email);
+		
+		if (email==null) { 
+			Log.i(TAG, "it is null?");
+			return;
+		} else {
+			Log.i(TAG, "it is NOT null");
+		}
 
 		// set saved value
 		final SharedPreferences sp = getSharedPreferences(PREF_FILE,
@@ -173,46 +186,112 @@ public class MainActivity extends FragmentActivity {
 			}
 		});
 	}
-	
+
 	protected void showDialog() {
-		DialogFragment newFragment = ErrorAlertDialogFragment.newInstance(this.validation_error);
+		DialogFragment newFragment = ErrorAlertDialogFragment
+				.newInstance(this.validation_error);
 		newFragment.show(getSupportFragmentManager(), "dialog");
 	}
 
 	public void sendMessage(View view) {
-		EditText email = (EditText) findViewById(R.id.edit_email);
-		EditText amount = (EditText) findViewById(R.id.edit_amount);
-		AutoCompleteTextView tags = (AutoCompleteTextView) findViewById(R.id.edit_tags);
-		AutoCompleteTextView desc = (AutoCompleteTextView) findViewById(R.id.edit_desc);
-		AutoCompleteTextView acct = (AutoCompleteTextView) findViewById(R.id.edit_acct);
-		
 		// validation
 		if (!this.validate()) {
-			//this.showDialog(DIALOG_VALIDATION);
 			this.showDialog();
 			return;
 		}
 
-		// save history before sending
-		autocompletes(desc.getText().toString(), PREF_DESC);
-		autocompletes(tags.getText().toString(), PREF_TAGS);
-		autocompletes(acct.getText().toString(), PREF_ACCT);
-		rule(desc.getText().toString(), tags.getText().toString());
+		// put new words, rules, etc
+		this.updateRules();
 
 		// update autocomplete adapters with new tags/descriptions
 		this.setupAdapters();
 
-		// comment
+	}
+
+	protected void pushTransactions() {
+		final SharedPreferences sp = getSharedPreferences(PREF_FILE,
+				MODE_PRIVATE);
+		
+		// deal to start email client
+		EditText email = (EditText) findViewById(R.id.edit_email);
 		Intent emailIntent = new Intent(Intent.ACTION_SEND);
 		emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] { email.getText()
 				.toString() });
 		emailIntent.putExtra(Intent.EXTRA_SUBJECT, "expense");
-		emailIntent.putExtra(Intent.EXTRA_TEXT, desc.getText().toString() + " "
-				+ amount.getText().toString() + " tags:"
-				+ tags.getText().toString() + " acct:" + acct.getText().toString());
+
+		String transactions = sp.getString(SAVED_TRANSACTIONS, "");
+
+		emailIntent.putExtra(Intent.EXTRA_TEXT, transactions);
 		emailIntent.setType("plain/text");
 		startActivity(Intent.createChooser(emailIntent,
 				getString(R.string.email_prompt)));
+		
+		// clear saved transactions
+		SharedPreferences.Editor edit = sp.edit();
+		edit.putString(SAVED_TRANSACTIONS, "");
+		edit.commit();		
+	}
+
+	public void saveTransaction(View view) {
+		Boolean pushing = view.getId() == R.id.button_push;
+		Boolean valid = validate();
+
+		// validation
+		if (!valid && !pushing) {
+			showDialog();
+			return;
+		}
+
+		// put new words, rules, etc
+		this.updateRules();
+
+		// update autocomplete adapters with new tags/descriptions
+		this.setupAdapters();
+
+		if (valid) {
+			String transaction = getTransactionString();
+
+			final SharedPreferences sp = getSharedPreferences(PREF_FILE,
+					MODE_PRIVATE);
+			String existing = sp.getString(SAVED_TRANSACTIONS, "");
+
+			existing = existing + transaction + "\n";
+
+			SharedPreferences.Editor edit = sp.edit();
+			edit.putString(SAVED_TRANSACTIONS, existing);
+			edit.commit();
+		}
+		
+		EditText amount = (EditText) findViewById(R.id.edit_amount);
+		amount.setText("");
+		amount.requestFocus();
+		
+		if (pushing) {
+			pushTransactions();
+		}
+	}
+
+	/**
+	 * Returns transaction string from current form data.
+	 * 
+	 * @return a string to insert in email message
+	 */
+	protected String getTransactionString() {
+		EditText amount = (EditText) findViewById(R.id.edit_amount);
+		AutoCompleteTextView tags = (AutoCompleteTextView) findViewById(R.id.edit_tags);
+		AutoCompleteTextView desc = (AutoCompleteTextView) findViewById(R.id.edit_desc);
+		AutoCompleteTextView acct = (AutoCompleteTextView) findViewById(R.id.edit_acct);
+
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:dd:ss",
+				Locale.US);
+		String now = format.format(new Date());
+
+		String result = desc.getText().toString() + " "
+				+ amount.getText().toString() + " tags:"
+				+ tags.getText().toString() + " acct:"
+				+ acct.getText().toString() + " date:" + now;
+		
+		return result;
 	}
 
 	protected void setupAdapters() {
@@ -221,16 +300,31 @@ public class MainActivity extends FragmentActivity {
 		AutoCompleteTextView acct = (AutoCompleteTextView) findViewById(R.id.edit_acct);
 
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, autocompletes(null, PREF_DESC));
+				android.R.layout.simple_list_item_1, autocompletes(null,
+						PREF_DESC));
 		desc.setAdapter(adapter);
 
 		adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, autocompletes(null, PREF_TAGS));
+				android.R.layout.simple_list_item_1, autocompletes(null,
+						PREF_TAGS));
 		tags.setAdapter(adapter);
 
 		adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, autocompletes(null, PREF_ACCT));
+				android.R.layout.simple_list_item_1, autocompletes(null,
+						PREF_ACCT));
 		acct.setAdapter(adapter);
+	}
+
+	protected void updateRules() {
+		AutoCompleteTextView tags = (AutoCompleteTextView) findViewById(R.id.edit_tags);
+		AutoCompleteTextView desc = (AutoCompleteTextView) findViewById(R.id.edit_desc);
+		AutoCompleteTextView acct = (AutoCompleteTextView) findViewById(R.id.edit_acct);
+
+		// save history before sending
+		autocompletes(desc.getText().toString(), PREF_DESC);
+		autocompletes(tags.getText().toString(), PREF_TAGS);
+		autocompletes(acct.getText().toString(), PREF_ACCT);
+		rule(desc.getText().toString(), tags.getText().toString());
 	}
 
 	protected String[] autocompletes(String new_item, String pref_id) {
@@ -263,51 +357,52 @@ public class MainActivity extends FragmentActivity {
 
 		return tag;
 	}
-	
+
 	protected Boolean validate() {
 		String tmp;
 		EditText edit;
-		
+
 		Boolean result = true;
-		
+
 		this.validation_error = "";
-		
+
 		// check amount
 		edit = (EditText) findViewById(R.id.edit_amount);
 		tmp = edit.getText().toString();
-		if (tmp==null || tmp.length()==0) {
+		if (tmp == null || tmp.length() == 0) {
 			this.validation_error += "\n";
 			this.validation_error = this.getString(R.string.error_amount_empty);
 			result = false;
 		}
-		
+
 		// check description
 		edit = (EditText) findViewById(R.id.edit_desc);
 		tmp = edit.getText().toString();
-		if (tmp==null || tmp.length()==0) {
+		if (tmp == null || tmp.length() == 0) {
 			this.validation_error += "\n";
 			this.validation_error += this.getString(R.string.error_desc_empty);
 			result = false;
 		}
-		
+
 		// check tags
 		edit = (EditText) findViewById(R.id.edit_tags);
 		tmp = edit.getText().toString();
-		if (tmp==null || tmp.length()==0) {
+		if (tmp == null || tmp.length() == 0) {
 			this.validation_error += "\n";
 			this.validation_error += this.getString(R.string.error_tags_empty);
 			result = false;
 		}
-		
+
 		// check account
 		edit = (EditText) findViewById(R.id.edit_acct);
 		tmp = edit.getText().toString();
-		if (tmp==null || tmp.length()==0) {
+		if (tmp == null || tmp.length() == 0) {
 			this.validation_error += "\n";
-			this.validation_error += this.getString(R.string.error_account_empty);
+			this.validation_error += this
+					.getString(R.string.error_account_empty);
 			result = false;
 		}
-		
+
 		return result;
 	}
 }
