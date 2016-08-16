dc61daf8824a1b3e4cf8bb4b48be6035b8115b19
Viacheslav Iutin <iutinvg@gmail.com>
2016-08-16 12:04:24 +0700

    initial build for android

--- base/android/jni/Android.mk
+++ base/android/jni/Android.mk
@@ -1,11 +1,15 @@
 LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := buxoff
+LOCAL_SRC_FILES := ../../Storage.cc ../../Record.cc ../../Email.cc
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../include $(LOCAL_PATH)/../../deps/catch/include $(LOCAL_PATH)/../../deps/json/src $(LOCAL_PATH)/../../deps/leveldb/include $(LOCAL_PATH)/../../deps/leveldb
+LOCAL_STATIC_LIBRARIES += leveldb
+include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-
-LOCAL_MODULE := slava
-# LOCAL_CPP_EXTENSION := .cc
-
-LOCAL_SRC_FILES := ../../slava.cc
-LOCAL_C_INCLUDES := ../../include
-
-include $(BUILD_SHARED_LIBRARY)
+LOCAL_MODULE := leveldb
+LOCAL_CFLAGS := -D_REENTRANT -DOS_ANDROID -DLEVELDB_PLATFORM_POSIX -DNDEBUG
+LOCAL_CPP_EXTENSION := .cc
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../deps/leveldb/include $(LOCAL_PATH)/../../deps/leveldb
+LOCAL_SRC_FILES := ../../deps/leveldb/db/builder.cc ../../deps/leveldb/db/c.cc ../../deps/leveldb/db/db_impl.cc ../../deps/leveldb/db/db_iter.cc ../../deps/leveldb/db/dbformat.cc ../../deps/leveldb/db/filename.cc ../../deps/leveldb/db/log_reader.cc ../../deps/leveldb/db/log_writer.cc ../../deps/leveldb/db/memtable.cc ../../deps/leveldb/db/repair.cc ../../deps/leveldb/db/table_cache.cc ../../deps/leveldb/db/version_edit.cc ../../deps/leveldb/db/version_set.cc ../../deps/leveldb/db/write_batch.cc ../../deps/leveldb/table/block.cc ../../deps/leveldb/table/block_builder.cc ../../deps/leveldb/table/filter_block.cc ../../deps/leveldb/table/format.cc ../../deps/leveldb/table/iterator.cc ../../deps/leveldb/table/merger.cc ../../deps/leveldb/table/table.cc ../../deps/leveldb/table/table_builder.cc ../../deps/leveldb/table/two_level_iterator.cc ../../deps/leveldb/util/arena.cc ../../deps/leveldb/util/bloom.cc ../../deps/leveldb/util/cache.cc ../../deps/leveldb/util/coding.cc ../../deps/leveldb/util/comparator.cc ../../deps/leveldb/util/crc32c.cc ../../deps/leveldb/util/env.cc ../../deps/leveldb/util/env_posix.cc ../../deps/leveldb/util/filter_policy.cc ../../deps/leveldb/util/hash.cc ../../deps/leveldb/util/histogram.cc ../../deps/leveldb/util/logging.cc ../../deps/leveldb/util/options.cc ../../deps/leveldb/util/status.cc ../../deps/leveldb/port/port_posix.cc
+include $(BUILD_STATIC_LIBRARY)
--- base/android/jni/Application.mk
+++ base/android/jni/Application.mk
@@ -1,2 +1,12 @@
-APP_ABI := all
-APP_STL := stlport_static
+# APP_ABI := all
+# APP_ABI := armeabi armeabi-v7a mips x86
+APP_ABI := armeabi
+# APP_STL := gnustl_static
+# APP_STL := stlport_static
+# APP_STL := c++_static
+# NDK_TOOLCHAIN_VERSION := clang
+# APP_CPPFLAGS += -std=c++14
+
+APP_STL := c++_shared
+NDK_TOOLCHAIN_VERSION := clang
+APP_CPPFLAGS += -frtti -fexceptions
