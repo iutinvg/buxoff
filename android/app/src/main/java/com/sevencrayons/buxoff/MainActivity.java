0070e360597f6de90c16b6589080a480e6d83e00
Viacheslav Iutin <iutinvg@gmail.com>
2017-01-17 10:04:48 +0700

    brand new android project

--- android/app/src/main/java/com/sevencrayons/buxoff/MainActivity.java
+++ android/app/src/main/java/com/sevencrayons/buxoff/MainActivity.java
@@ -1,41 +1,84 @@
 package com.sevencrayons.buxoff;
 
 import android.os.Bundle;
-import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textCount;
-    Button buttonAdd;
     Buxoff buxoff;
+    TextView textCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        buxoff = new Buxoff();
-        buxoff.init(getDocPath());
+        textCount = (TextView) findViewById(R.id.textCount);
 
-        textCount = (TextView) findViewById(R.id.text);
-        buttonAdd = (Button) findViewById(R.id.button);
-
-        buttonAdd.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
+                Snackbar.make(view, "One record added", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
                 updateCount();
             }
         });
+
+        buxoff = new Buxoff();
+        buxoff.init(getDocPath());
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        buxoff = new Buxoff();
+//        buxoff.init(getDocPath());
+//
+//        textCount = (TextView) findViewById(R.id.textView);
+//        buttonAdd = (Button) findViewById(R.id.button);
+//
+//        buttonAdd.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                updateCount();
+//            }
+//        });
+//    }
 
     protected String getDocPath() {
         File data = getCacheDir();
@@ -45,6 +88,8 @@ public class MainActivity extends AppCompatActivity {
 
     protected void updateCount() {
         buxoff.add();
+        buxoff.count();
         textCount.setText("Total: " + buxoff.count());
     }
+
 }
