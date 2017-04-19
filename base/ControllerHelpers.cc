c54c39f976a0d819e07e8c53bf09cc6eb50653f9
Viacheslav Iutin <iutinvg@gmail.com>
2017-04-19 17:13:46 +0700

    catch new exception in controller

--- base/ControllerHelpers.cc
+++ base/ControllerHelpers.cc
@@ -10,9 +10,17 @@ void Buxoff::controller_add(Connection* c, const Record& r)
 {
     r.validate();
     RecordStorage rs{c};
-    rs.put(r);
     TagsStorage ts{c};
-    ts.put_all(r.tags());
+
+    try {
+        rs.put(r);
+        ts.put_all(r.tags());
+    } catch (StorageError& e) {
+        // from user point of view, it's totally fine
+        // to store empty tags or descriptions
+        // so we just swallow this type of exceptions
+        // because all we want is just avoid storing empty strings
+    }
 }
 
 std::string Buxoff::controller_push(Connection* c, const Record& r)
