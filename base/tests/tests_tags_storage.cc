ea94f50647aadacbd7ec69c1c4e6ca9a589b4bc1
Viacheslav Iutin <iutinvg@gmail.com>
2017-04-26 18:54:04 +0700

    failed test which display some misunderstanding of methods resolutions + confusing with names

--- base/tests/tests_tags_storage.cc
+++ base/tests/tests_tags_storage.cc
@@ -20,3 +20,16 @@ TEST_CASE("put", "[tags-storage]") {
     REQUIRE(m.size() == 1);
     REQUIRE(m[val] == val);
 }
+
+TEST_CASE("put-via-put-all", "[tags-storage]") {
+    clean_storage();
+    auto c = Connection("test.db");
+    auto val = random_key();
+    auto ss = TagsStorage(&c);
+    // put the same tag twice
+    ss.put_all(vector<string>{val, val});
+    // but it's stored once
+    auto m = ss.all_map(true);
+    REQUIRE(m.size() == 1);
+    REQUIRE(m[val] == val);
+}
