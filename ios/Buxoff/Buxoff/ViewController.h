0e3f9ed1ce7525e24ea75d4e7382a60bbe173087
Viacheslav Iutin <iutinvg@gmail.com>
2017-05-08 08:42:20 +0700

    screen changes for ios

--- ios/Buxoff/Buxoff/ViewController.h
+++ ios/Buxoff/Buxoff/ViewController.h
@@ -16,11 +16,16 @@
 
 @property (nonatomic, strong) IBOutlet UITextField *textAmount;
 @property (nonatomic, strong) IBOutlet UITextField *textDesc;
+@property (nonatomic, strong) IBOutlet UITextField *textTags;
 @property (nonatomic, strong) IBOutlet UITextField *textAcct;
+@property (nonatomic, strong) IBOutlet UITextField *textEmail;
+
 @property (nonatomic, strong) IBOutlet UIButton *buttonAdd;
+@property (nonatomic, strong) IBOutlet UIButton *buttonPush;
+
 @property (nonatomic, strong) IBOutlet UILabel *labelCounter;
 
 - (IBAction)actionAdd:(id)sender;
+- (IBAction)actionPush:(id)sender;
 
 @end
-
